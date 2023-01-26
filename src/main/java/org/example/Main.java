96247009b4dbeb4c095ca8305e95083f2eb90337
JoseaSosa <josea_1998@htomail.com>
2023-01-26 12:08:14 -0500

    se crea proyecto fiestas

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //EN EL MAIN LLAMO A MIS CLASES
+        //EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
+        //EN EL MAIN CREO UNA INSTANCIA DE MIS CLASES
+        //EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
+        int numeroHijos=10;
+
+        Invitado objetoDeClaseInvitado=new Invitado();
+        Invitado objeto2=new Invitado(33,true,"Juan",10000);
+        Invitado objeto3=new Invitado(nombre:Marta);
+
+
+        //obtener el valor de los atributos
+        System.out.println(objetoDeClaseInvitado.nombre);
+        System.out.println(objeto2.nombre);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
